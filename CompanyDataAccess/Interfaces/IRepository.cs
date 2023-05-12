e285531712c26efa9b02146c972ffb8c3aa28139
AdilTehranli <adiljt@code.edu.az>
2023-05-12 19:13:11 +0400

    crated

---
+++ CompanyDataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyDataAccess.Interfaces
+{
+    internal class IRepository
+    {
+    }
+}
